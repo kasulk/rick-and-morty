79c62f073007ecfa7b2567618ccc80bfa697113f
kasi <danielkaser@mailbox.org>
2023-05-11 16:24:41 +0200

    finish task 3 pagination

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 const maxPage = 1;
-const page = 1;
+let page = 1;
 const searchQuery = "";
 
-async function fetchCharacters() {
+async function fetchCharacters(page) {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?page=${page}`
+    );
     if (response.ok) {
       cardContainer.innerHTML = "";
       const data = await response.json();
@@ -33,24 +35,31 @@ async function fetchCharacters() {
 
 // console.log(await fetchCharacters());
 
-const fetchedCharactersObject = await fetchCharacters();
-const fetchedCharacters = fetchedCharactersObject.results;
+let fetchedCharactersObject = "";
 
-// console.log(fetchedCharacters);
+async function renderCharacters(page) {
+  fetchedCharactersObject = await fetchCharacters(page);
+  let fetchedCharacters = fetchedCharactersObject.results;
 
-const characterHTML = fetchedCharacters.map((character) => {
-  const characterCard = createCharacterCard(
-    character.name,
-    character.status,
-    character.type,
-    character.episode.length,
-    character.image
-  );
-  return characterCard;
-});
+  // console.log(fetchedCharacters);
 
-// console.log(characterHTML);
-cardContainer.innerHTML = characterHTML;
+  const characterHTML = fetchedCharacters.map((character) => {
+    const characterCard = createCharacterCard(
+      character.name,
+      character.status,
+      character.type,
+      character.episode.length,
+      character.image
+    );
+    return characterCard;
+  });
+
+  // console.log(characterHTML);
+  cardContainer.innerHTML = characterHTML;
+
+  // return fetchedCharactersObject;
+}
+await renderCharacters();
 
 // testPageFetch
 const testPageFetchData = async function testPageFetch() {
@@ -58,11 +67,11 @@ const testPageFetchData = async function testPageFetch() {
     await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
   ).json();
 };
-console.log("testFetch:", await testPageFetchData());
+// console.log("testFetch:", await testPageFetchData());
 
 let pageIndex = page;
 // Add an event listener on each of the next and prev buttons which do the following
-nextButton.addEventListener("click", (event) => {
+nextButton.addEventListener("click", async (event) => {
   // it is prevented that the page index could go higher than the max page index or below 1
   pageIndex++;
   pagination.innerHTML = pageIndex + " / " + fetchedCharactersObject.info.pages;
@@ -71,8 +80,14 @@ nextButton.addEventListener("click", (event) => {
     nextButton.disabled = true;
   }
   prevButton.disabled = false;
+
+  // await fetchCharacters(5);
+  // console.log("fetchCharacters:", await fetchCharacters(6));
+  await renderCharacters(pageIndex);
+  // console.log("renderCharacters(5):", await renderCharacters(5));
 });
-prevButton.addEventListener("click", (event) => {
+
+prevButton.addEventListener("click", async (event) => {
   // it is prevented that the page index could go higher than the max page index or below 1
   pageIndex--;
   pagination.innerHTML = pageIndex + " / " + fetchedCharactersObject.info.pages;
@@ -81,6 +96,8 @@ prevButton.addEventListener("click", (event) => {
     prevButton.disabled = true;
   }
   nextButton.disabled = false;
+
+  await renderCharacters(pageIndex);
 });
 
 // the page index is increased / decreased
